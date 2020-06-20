4d1edaa83f80aa78ae6aa12c63909e02e4efd102
Gunhoon Lee <gunhoon@gmail.com>
2020-06-21 01:32:26 +0900

    발매일이 playlist의 updt_date보다 늦은 곡 처리

--- model/victoria.py
+++ model/victoria.py
@@ -9,8 +9,13 @@ class Victoria:
         self._main_model = MatrixFactorization(song_meta_json, song_topk, tag_topk)
         self._fall_model = Fallback(song_topk, tag_topk)
 
+        self._issue_song = set() # 예외처리를 위해 발매일자가 잘못된 곡 저장
+        self._issue_date = {song['id'] : song['issue_date'] for song in song_meta_json}
+
 
     def fit(self, train, val):
+        #self._find_issue_song(train)
+
         self._main_model.fit(train, val)
         self._fall_model.fit(train)
 
@@ -24,7 +29,42 @@ class Victoria:
         if len(t_pred) == 0:
             _, t_pred = self._fall_model.predict(playlist)
 
-        s_rec = [k for k, v in s_pred]
+        s_rec = self._rearrange_song(s_pred, playlist)
         t_rec = [k for k, v in t_pred]
 
         return s_rec, t_rec
+
+
+    # 곡의 issue_date가 플레이리스트의 updt_date보다 늦은 곡 찾기
+    def _find_issue_song(self, train):
+        for p in train:
+            updt_date = int(self._get_update_date(p))
+
+            for sid in p['songs']:
+                if int(self._issue_date[sid]) > updt_date:
+                    self._issue_song.add(sid)
+
+
+    # playlist의 updt_date와 song의 issue_date를 비교해서 재배치
+    def _rearrange_song(self, s_pred, playlist):
+        s_rec = []
+        s_tmp = []
+        updt_date = int(self._get_update_date(playlist))
+
+        for sid, _ in s_pred:
+            if int(self._issue_date[sid]) > updt_date and sid not in self._issue_song:
+                s_tmp.append(sid)
+            else:
+                s_rec.append(sid)
+
+        s_rec.extend(s_tmp)
+        return s_rec
+
+
+    # playlist의 updt_date
+    def _get_update_date(self, playlist):
+        updt_date = playlist['updt_date'][0:4] + \
+                    playlist['updt_date'][5:7] + \
+                    playlist['updt_date'][8:10]
+
+        return updt_date
