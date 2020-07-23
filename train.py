4956816673b87939e1fdbb5bd2051952d8492743
Gunhoon Lee <gunhoon@gmail.com>
2020-07-24 02:34:47 +0900

    규정 변경으로 val.json과 test.json 모두 사용

--- train.py
+++ train.py
@@ -6,19 +6,29 @@ from baseline.arena_util import load_json
 from model.victoria import Victoria
 
 
-def main(song_meta_fname, train_fname, question_fname):
+def main(song_meta_fname, train_fname, val_fname, test_fname, question_fname):
     print('Loading song meta...')
     song_meta_json = load_json(song_meta_fname)
 
     print('Loading train file...')
     train = load_json(train_fname)
 
-    print('Loading question file...')
-    question = load_json(question_fname)
+    # 규정이 변경되어, val.json, test.json 모두 loading함.
+    print('Loading val file...')
+    val = load_json(val_fname)
 
-    print('Training a model...')
+    print('Loading test file...')
+    test = load_json(test_fname)
+
+    if question_fname == val_fname or question_fname == test_fname:
+        question = []
+    else:
+        print('Loading question file...')
+        question = load_json(question_fname)
+
+    print('Start training...')
     model = Victoria(song_meta_json)
-    model.fit(train, question)
+    model.fit(train, val + test + question)
 
     print('Saving the model...')
     with open('victoria.pkl', 'wb') as f:
@@ -35,8 +45,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--song_meta_fname', default='res/song_meta.json')
     parser.add_argument('--train_fname', default='res/train.json')
+    parser.add_argument('--val_fname', default='res/val.json')
+    parser.add_argument('--test_fname', default='res/test.json')
     parser.add_argument('--question_fname', default='res/test.json')
     args = parser.parse_args()
 
     print(args)
-    main(args.song_meta_fname, args.train_fname, args.question_fname)
+    main(args.song_meta_fname, args.train_fname, args.val_fname, args.test_fname, args.question_fname)
