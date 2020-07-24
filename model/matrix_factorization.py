d863251f95f3b5788c3c993824b8f1c56c2fcd7e
Gunhoon Lee <gunhoon@gmail.com>
2020-07-24 10:23:07 +0900

    song과 tag의 코드 분리

--- model/matrix_factorization.py
+++ model/matrix_factorization.py
@@ -14,16 +14,25 @@ class MatrixFactorization:
         self._s_best = None
         self._t_best = None
 
-        self._data = PreferenceData(song_meta)
+        self._s_data = PreferenceData(song_meta, song_type=True)
+        self._t_data = PreferenceData(song_meta, song_type=False)
 
 
     def fit(self, train, val):
-        df = self._data.get_preference(train, val)
+        print("Training song model...")
+        self._fit_for_song(train, val)
 
-        s_len = self._data.get_song_length()
-        t_len = self._data.get_tag_length()
-        k_len = self._data.get_keyword_length()
-        e_len = self._data.get_extension_length()
+        print("Training tag model...")
+        self._fit_for_tag(train, val)
+
+
+    def _fit_for_song(self, train, val):
+        df = self._s_data.get_preference(train, val)
+
+        s_len = self._s_data.get_song_length()
+        t_len = self._s_data.get_tag_length()
+        k_len = self._s_data.get_keyword_length()
+        e_len = self._s_data.get_extension_length()
 
         # user x item csr_matrix
         user_item_csr = sparse.csr_matrix((df['preference'].astype(float), (df['user_id'], df['item_id'])))
@@ -40,6 +49,20 @@ class MatrixFactorization:
 
         self._s_best = s_model.recommend_all(user_song_csr[:, :s_len], N=self._s_topk)
 
+
+    def _fit_for_tag(self, train, val):
+        df = self._t_data.get_preference(train, val)
+
+        s_len = self._t_data.get_song_length()
+        t_len = self._t_data.get_tag_length()
+        k_len = self._t_data.get_keyword_length()
+        e_len = self._t_data.get_extension_length()
+
+        # user x item csr_matrix
+        user_item_csr = sparse.csr_matrix((df['preference'].astype(float), (df['user_id'], df['item_id'])))
+        user_song_csr = user_item_csr[:, :s_len + t_len + k_len + e_len]
+        user_tags_csr = user_item_csr
+
         print("Training tag model...")
         t_model = AlternatingLeastSquares(factors=420)
         t_model.fit(user_tags_csr.T * 65)
@@ -52,19 +75,23 @@ class MatrixFactorization:
 
 
     def predict(self, playlist):
-        user_id = self._data.get_pid_to_uid(playlist['id'])
-
-        s_len = self._data.get_song_length()
-
         s_pred = []
         t_pred = []
+
+        user_id = self._s_data.get_pid_to_uid(playlist['id'])
+        s_len = self._s_data.get_song_length()
 
         if user_id != None:
             # s_best 는 (item_id, score)의 list, 이것을 (sid, score) 의 list로 변경
             for i, item_id in enumerate(self._s_best[user_id]):
-                s_pred.append((self._data.get_iid_to_sid(int(item_id)), self._s_topk - i))
+                s_pred.append((self._s_data.get_iid_to_sid(int(item_id)), self._s_topk - i))
 
+
+        user_id = self._t_data.get_pid_to_uid(playlist['id'])
+        s_len = self._t_data.get_song_length()
+
+        if user_id != None:
             for i, item_id in enumerate(self._t_best[user_id]):
-                t_pred.append((self._data.get_iid_to_tag(int(item_id) + s_len), self._t_topk - i))
+                t_pred.append((self._t_data.get_iid_to_tag(int(item_id) + s_len), self._t_topk - i))
 
         return s_pred, t_pred
--- model/preference_data.py
+++ model/preference_data.py
@@ -6,8 +6,10 @@ from model.tokenizer import Tokenizer
 
 
 class PreferenceData:
-    def __init__(self, song_meta):
+    def __init__(self, song_meta, song_type=True):
         self._song_meta = song_meta
+        # song과 tag를 따로 분리하기 위해(성능 향상에 차이가 있어서)
+        self._song_type = song_type
 
         self._s_num = 0
         self._t_num = 0
@@ -51,8 +53,16 @@ class PreferenceData:
         return self._pid_to_uid.get(plylst_id)
 
 
-    # Column(plylst_id, sid, tag, confidence, user_id, item_id)의 dataframe
     def get_preference(self, train, val):
+        if self._song_type:
+            df = self._get_preference_for_song(train, val)
+        else:
+            df = self._get_preference_for_tag(train, val)
+
+        return df
+
+
+    def _get_preference_for_song(self, train, val):
         # 빈도가 적은 것은 feature에서 제거하기 위한 counter
         s_c = Counter()
         t_c = Counter()
@@ -62,7 +72,7 @@ class PreferenceData:
             t_c.update(p['tags'])
 
         s_table, t_table, k_table, e_table, g_table = \
-                self._preference_table(train + val, s_c, t_c)
+                self._preference_table_for_song(train + val, s_c, t_c)
 
         # song, tag, keyword, extenstion, genre of train
         s_df = pd.DataFrame(s_table, columns =['plylst_id', 'sid', 'preference'])
@@ -109,7 +119,101 @@ class PreferenceData:
         return df[['user_id', 'item_id', 'preference']].reset_index(drop=True)
 
 
-    def _preference_table(self, playlists, s_counter, t_counter):
+    def _get_preference_for_tag(self, train, val):
+        # 빈도가 적은 것은 feature에서 제거하기 위한 counter
+        s_c = Counter()
+        t_c = Counter()
+ 
+        for p in train + val:
+            s_c.update(p['songs'])
+            t_c.update(p['tags'])
+
+        s_table, t_table, k_table, e_table, g_table = \
+                self._preference_table_for_tag(train + val, s_c, t_c)
+
+        # song, tag, keyword, extenstion, genre of train
+        s_df = pd.DataFrame(s_table, columns =['plylst_id', 'sid', 'preference'])
+        t_df = pd.DataFrame(t_table, columns =['plylst_id', 'tag', 'preference'])
+        k_df = pd.DataFrame(k_table, columns =['plylst_id', 'key', 'preference'])
+        e_df = pd.DataFrame(e_table, columns =['plylst_id', 'ext', 'preference'])
+        g_df = pd.DataFrame(g_table, columns =['plylst_id', 'gnr', 'preference'])
+
+        s_df['item_id'] = s_df['sid'].astype('category').cat.codes
+        t_df['item_id'] = t_df['tag'].astype('category').cat.codes
+        k_df['item_id'] = k_df['key'].astype('category').cat.codes
+        e_df['item_id'] = e_df['ext'].astype('category').cat.codes
+        g_df['item_id'] = g_df['gnr'].astype('category').cat.codes
+
+        # item_id에서 sid를 찾기 위한 dictionary 생성
+        new_df = s_df[['item_id', 'sid']].drop_duplicates().reset_index(drop=True)
+        for _, r in new_df.iterrows():
+            self._iid_to_sid.update({r['item_id']: r['sid']})
+        # item_id에서 tag를 찾기 위한 dictionary 생성
+        new_df = t_df[['item_id', 'tag']].drop_duplicates().reset_index(drop=True)
+        for _, r in new_df.iterrows():
+            self._iid_to_tag.update({r['item_id']: r['tag']})
+
+        self._s_num = s_df['item_id'].nunique()
+        self._t_num = t_df['item_id'].nunique()
+        self._k_num = k_df['item_id'].nunique()
+        self._e_num = e_df['item_id'].nunique()
+
+        # tags의 item_id는 songs의 item_id 이후 번호 부터 부여한다.
+        t_df['item_id'] = t_df['item_id'] + self._s_num
+        k_df['item_id'] = k_df['item_id'] + self._s_num + self._t_num
+        e_df['item_id'] = e_df['item_id'] + self._s_num + self._t_num + self._k_num
+        g_df['item_id'] = g_df['item_id'] + self._s_num + self._t_num + self._k_num + self._e_num
+
+        df = pd.concat([s_df, t_df, k_df, e_df, g_df])
+
+        df['user_id'] = df['plylst_id'].astype('category').cat.codes
+
+        # plylst_id를 user_id를 찾기 위한 dictionary 생성
+        new_df = df[['plylst_id', 'user_id']].drop_duplicates().reset_index(drop=True)
+        for _, r in new_df.iterrows():
+            self._pid_to_uid.update({r['plylst_id']: r['user_id']})
+
+        return df[['user_id', 'item_id', 'preference']].reset_index(drop=True)
+
+
+    def _preference_table_for_song(self, playlists, s_counter, t_counter):
+        s_table = []
+        t_table = []
+        k_table = []
+        e_table = []
+        g_table = []
+
+        api = Tokenizer()
+
+        for p in tqdm(playlists):
+            # songs
+            for sid in p['songs']:
+                if s_counter[sid] > 1:
+                    s_table.append((p['id'], sid, 1))
+                # 3 이하는 참조는 하는데, 추천은 하지 않음.
+                #elif s_counter[sid] > 1:
+                #    e_table.append((p['id'], sid, 1))
+
+            # tags
+            for tag in p['tags']:
+                if t_counter[tag] > 1:
+                    t_table.append((p['id'], tag, 1))
+
+            # keyword
+            if len(p['plylst_title']) > 0:
+                keyword = api.tokenize(p['plylst_title'])
+                for k in keyword:
+                    k_table.append((p['id'], k, 1))
+
+            # detail genre
+            gnr = self._get_dtl_genre(p['songs'])
+            for k, v in gnr:
+                g_table.append((p['id'], k, v))
+
+        return s_table, t_table, k_table, e_table, g_table
+
+
+    def _preference_table_for_tag(self, playlists, s_counter, t_counter):
         s_table = []
         t_table = []
         k_table = []
